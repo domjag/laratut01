9a39162ea95d6e5866bd758616bc9ed36b4f4b39
domjag <domjag@outlook.com>
2023-07-11 15:15:09 -0400

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+  <h1>Hi Guys!</h1>  
+  THanks for coming to my about us page. I am going to help you develop a laravel website.
+</body>
+</html>
